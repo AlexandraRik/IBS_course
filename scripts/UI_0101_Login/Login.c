ea59d3e43d4cedae6f694f9ffaecb323cadc97bd
AlexandraRik <89975263+AlexandraRik@users.noreply.github.com>
2021-09-09 16:23:39 +0300

    0101

---
+++ scripts/UI_0101_Login/Login.c
@@ -0,0 +1,74 @@
+Login()
+{
+	
+
+	
+	lr_start_transaction("0101_Login");
+	
+
+
+  	lr_start_transaction("WebTourConnection"); 
+
+	web_reg_find("Text/IC=Web Tours",
+		LAST);
+
+	
+	web_reg_save_param("userSession",
+		"LB=name=\"userSession\" value=\"",
+		"RB=\"",
+		LAST);
+
+	web_url("WebTours", 
+		"URL=http://127.0.0.1:1080/WebTours/", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t3.inf", 
+		"Mode=HTML", 
+		LAST);
+	
+
+	web_set_sockets_option("SSL_VERSION", "2&3");
+
+	lr_end_transaction("WebTourConnection", LR_AUTO);
+	
+	lr_think_time(5);
+	
+	
+	lr_start_transaction("Login");
+	
+   
+
+	
+
+	web_reg_find("Text/IC=User password was correct",
+		LAST);
+	web_reg_find("Text/IC={Username}", LAST);
+	
+	web_submit_data("login.pl",
+		"Action=http://127.0.0.1:1080/cgi-bin/login.pl",
+		"Method=POST",
+		"TargetFrame=body",
+		"RecContentType=text/html",
+		"Referer=http://127.0.0.1:1080/cgi-bin/nav.pl?in=home",
+		"Snapshot=t4.inf",
+		"Mode=HTML",
+		ITEMDATA,
+		"Name=userSession", "Value={userSession}", ENDITEM,
+		"Name=username", "Value={Username}", ENDITEM,
+		"Name=password", "Value={Password}", ENDITEM,
+		"Name=JSFormSubmit", "Value=off", ENDITEM,
+		"Name=login.x", "Value=55", ENDITEM,
+		"Name=login.y", "Value=3", ENDITEM,
+		LAST);
+
+	lr_end_transaction("Login",LR_AUTO);
+	
+	
+	
+	lr_end_transaction("0101_Login", LR_AUTO);
+
+
+	return 0;
+}
---
+++ scripts/UI_0101_Login/data/Login.c
@@ -0,0 +1,40 @@
+Login()
+{
+
+	web_url("WebTours", 
+		"URL=http://127.0.0.1:1080/WebTours/", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t3.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_set_sockets_option("SSL_VERSION", "2&3");
+
+	lr_think_time(8);
+
+	lr_start_transaction("1_Login");
+
+	web_submit_data("login.pl", 
+		"Action=http://127.0.0.1:1080/cgi-bin/login.pl", 
+		"Method=POST", 
+		"TargetFrame=body", 
+		"RecContentType=text/html", 
+		"Referer=http://127.0.0.1:1080/cgi-bin/nav.pl?in=home", 
+		"Snapshot=t4.inf", 
+		"Mode=HTML", 
+		ITEMDATA, 
+		"Name=userSession", "Value=132076.569785338zHQDziipffiDDDDDtAHzcpVDVDf", ENDITEM, 
+		"Name=username", "Value=jojo", ENDITEM, 
+		"Name=password", "Value=bean", ENDITEM, 
+		"Name=JSFormSubmit", "Value=off", ENDITEM, 
+		"Name=login.x", "Value=55", ENDITEM, 
+		"Name=login.y", "Value=3", ENDITEM, 
+		LAST);
+
+	lr_end_transaction("1_Login",LR_AUTO);
+
+	return 0;
+}
